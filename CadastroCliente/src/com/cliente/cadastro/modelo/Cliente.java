3150bc3e4e94250599341ab1d62b04bf2cc5abac
Jaime Gonzalez <68569736+jaime-gonzalez@users.noreply.github.com>
2020-09-01 02:45:44 -0300

    Create Cliente.java

---
+++ CadastroCliente/src/com/cliente/cadastro/modelo/Cliente.java
@@ -0,0 +1,22 @@
+package com.cliente.cadastro.modelo;
+
+public class Cliente {
+	
+	private Long codigo;
+	private String nome;
+	
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
