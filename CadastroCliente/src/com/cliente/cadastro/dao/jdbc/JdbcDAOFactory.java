a85b3db3875db5b7336ad0de14f08b221a0b841f
Jaime Gonzalez <68569736+jaime-gonzalez@users.noreply.github.com>
2020-09-01 02:48:25 -0300

    Create JdbcDAOFactory.java

---
+++ CadastroCliente/src/com/cliente/cadastro/dao/jdbc/JdbcDAOFactory.java
@@ -0,0 +1,28 @@
+package com.cliente.cadastro.dao.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import com.cliente.cadastro.dao.ClienteDAO;
+import com.cliente.cadastro.dao.DAOFactory;
+
+public class JdbcDAOFactory extends DAOFactory {
+
+	private Connection connection;
+
+	public JdbcDAOFactory() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			this.connection = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/cadastro_cliente?useTimezone=true&serverTimezone=UTC", "root",
+					"jvbn42.klpo19");
+		} catch (Exception e) {
+			throw new RuntimeException("Erro recuperando conexão com o banco de dados", e);
+		}
+	}
+
+	@Override
+	public ClienteDAO getClienteDAO() {
+		return new JdbcClienteDAO(connection);
+	}
+}
